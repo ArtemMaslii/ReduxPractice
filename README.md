a38c5a4df8f3882eb68b6031e795d5ce98a6c900
Artem <maslijartei@gmail.com>
2023-12-06 14:30:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is test project with hero admin panel!
+
+If you want to test it out, download it, run "npm i" and "npm start".
---
+++ src/actions/index.js
@@ -0,0 +1,58 @@
+export const heroesFetching = () => {
+    return {
+        type: 'HEROES_FETCHING'
+    }
+}
+
+export const filtersFetching = () => {
+    return {
+        type: "FILTERS_FETCHING"
+    }
+}
+
+export const heroesFetched = (heroes) => {
+    return {
+        type: 'HEROES_FETCHED',
+        payload: heroes
+    }
+}
+
+export const filtersFetched = (filters) => {
+    return {
+        type: 'FILTERS_FETCHED',
+        payload: filters
+    }
+}
+
+export const heroesFetchingError = () => {
+    return {
+        type: 'HEROES_FETCHING_ERROR'
+    }
+}
+
+export const filtersFetchingError = () => {
+    return {
+        type: 'FILTERS_FETCHING_ERROR'
+    }
+}
+
+export const activeFilterChanged = (filter) => {
+    return {
+        type: "ACTIVE_FILTER_CHANGED",
+        payload: filter
+    }
+}
+
+export const heroDeleted = (id) => {
+    return {
+        type: "HERO_DELETED",
+        payload: id
+    }
+}
+
+export const heroAdded = (newHero) => {
+    return {
+        type: "HERO_ADDED",
+        payload: newHero
+    }
+}
