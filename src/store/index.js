558d9963887144f0beeaff7d2ab2f4d83fa4bcb4
Artem <maslijartei@gmail.com>
2023-12-07 13:42:17 +0100

    Add redux deftools

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { legacy_createStore as createStore, combineReducers } from 'redux';
+import { legacy_createStore as createStore, combineReducers, compose } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -17,8 +17,12 @@ const enhancer = (createStore) => (...args) => {
     return store;
 }
 
-const store = createStore(combineReducers({heroes, filters}), enhancer);
+const store = createStore(
+    combineReducers({heroes, filters}),
+    compose(
+        enhancer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
 export default store;
-
-//    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
